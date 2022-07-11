1a98b3d24ae309e7ea79876231b24ca6b16598cd
erwinschrodinger1 <sunnynirjal@gmail.com>
2022-07-11 22:58:43 +0545

    promo app

---
+++ ARTreasureHuntPromo/Assets/Script/ARTapToPlaceObject.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.Experimental.XR;
+using System;
+
+public class ARTapToPlaceObject : MonoBehaviour
+{
+    public GameObject plane;
+    public GameObject placementIndicator;
+    public GameObject objectToPlace;
+    private ARRaycastManager aRRaycastManager;
+    private Pose placementPose;
+    private bool placementPoseIsValid = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        aRRaycastManager = FindObjectOfType<ARRaycastManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+
+        if(placementPoseIsValid&& Input.touchCount>0&& Input.GetTouch(0).phase == TouchPhase.Began){
+            PlaceObject();
+        }
+    }
+
+    private void PlaceObject(){
+        Instantiate(plane, placementPose.position, placementPose.rotation);
+        Instantiate(objectToPlace,new Vector3(placementPose.position.x, (placementPose.position.y +1),placementPose.position.z), new Quaternion(placementPose.rotation.x-0.7071f,placementPose.rotation.y,placementPose.rotation.z,placementPose.rotation.w+0.7071f));
+    }
+    private void UpdatePlacementIndicator()
+    {
+        if (placementPoseIsValid)
+        {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+        }
+        else
+        {
+            placementIndicator.SetActive(false);
+        }
+    }
+
+    private void UpdatePlacementPose()
+    {
+        var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        var hits = new List<ARRaycastHit>();
+        aRRaycastManager.Raycast(screenCenter, hits, UnityEngine.XR.ARSubsystems.TrackableType.Planes);
+        placementPoseIsValid = hits.Count > 0;
+        if (placementPoseIsValid)
+        {
+            placementPose = hits[0].pose;
+            var cameraForward = Camera.current.transform.forward;
+            var cameraBearing = new Vector3(cameraForward.x,0,cameraForward.z).normalized;
+            placementPose.rotation = Quaternion.LookRotation(cameraBearing);
+        }
+
+    }
+}
